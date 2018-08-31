3a63ebe13a611a2b1a2bfdf3e8f38ee9c0c75cb6
Kso <kasuo.takumi@gmail.com>
2018-08-31 22:50:05 +0900

    update

--- sql-injection/function.php
+++ sql-injection/function.php
@@ -1,21 +1,21 @@
 <?php
 
-//dsn
-function get_dsn(){
+//dbh
+function get_dbh(){
     
     //初期化
-    static $dsn = null;
+    static $dbh = null;
     //すでに読み込んでいる場合には処理をスキップ
-    if ($dsn === null) {
-        //MySQL Login information
+    if ($dbh === null) {
         $db_user = "root";
         $db_pass = "";
         $db_host = "localhost";
         $db_name = "test";
         $db_type = "mysql";
-        
+        //複文のSQLを無効・動的プレースホルダを無効
+        $option = array("PDO::MYSQL_ATTR_MULTI_STATEMENTS => false,PDO::ATTR_EMULATE_PREPARES => false");
         //データソースネームを使って接続
-        $dsn = new PDO("$db_type:hots=$db_host;dbname=$db_name;charset=utf8mb4", $db_user, $db_pass);
+        $dbh = new PDO("$db_type:hots=$db_host;dbname=$db_name;charset=utf8mb4", $db_user, $db_pass, $option);
     }
-    return $dsn;
+    return $dbh;
 }
--- sql-injection/session.php
+++ sql-injection/session.php
@@ -2,22 +2,37 @@
 
 include_once('./function.php');
 
-$dsn = get_dsn();
+$dbh = get_dbh();
 
 //ユーザーからの情報取得
 $user_id = $_POST['id'];
 $password = $_POST['pass'];
 
-//SQLの組み立てと実行
-$sql = "SELECT * FROM users WHERE user_id = '$user_id' AND user_pass = '$password'";
-echo $sql;
-echo '<br>';
-$check = $dsn->query($sql);
-$n = $check->rowCount();
+//IDとパスワードが正規表現にマッチしているか調べる
+if(preg_match('/[a-zA-Z0-9]{3,16}/',$user_id)&&preg_match('/[a-zA-Z0-9]{8,16}/',$password)){
 
-//ログイン判定
-if($n > 0){
-    echo "ログイン成功";
+    //プレースホルダ
+    $check = $dbh->prepare('SELECT id FROM users WHERE user_id = :id AND user_pass = :pass');
+    //バインドする値
+    $check->bindValue(':id',$user_id);
+    $check->bindValue(':pass',$password);
+    //SQLの実行
+    $check->execute();
+    //配列として値を取得
+    @$data_array = $check->fetchAll(PDO::FETCH_ASSOC);
+
+    $count = count($data_array);
+
+    //ログイン判定
+    if($count > 0){
+        echo "ログイン成功";
+    }else{
+        echo "ログイン失敗";
+    }
+
 }else{
-    echo "ログイン失敗";
+    //マッチしなかった場合
+    echo '不正な入力値です';
 }
+
+
